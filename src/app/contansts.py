e26b852900e027f3aa7471da1dc37296472d427d
Bruno Botelho <bruno.botelho.br@gmail.com>
2023-07-02 16:55:37 -0300

    debug

---
+++ src/app/contansts.py
@@ -0,0 +1,17 @@
+"""Constants."""
+import logging
+from enum import Enum
+
+# TTL Default value
+TTL = int(64)
+
+# Timeout default value, in seconds
+TIMEOUT = int(1)
+
+# Payload default value
+PAYLOAD: bytes = b"0123456789ABCDEF"
+
+# Logging level
+LOG_LEVEL = logging.CRITICAL
+
+# Enum for commands
